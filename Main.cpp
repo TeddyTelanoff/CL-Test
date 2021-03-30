03166ffd942311772000e663264a85aa48f339b5
Treidex <treidexsemextetry@gmail.com>
2021-03-30 15:43:56 -0700

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#include <CL/cl.h>
+
+static const char *clGetErrorString(cl_int error)
+{
+	switch (error)
+	{
+// run-time and JIT compiler errors
+	case 0: return "CL_SUCCESS";
+	case -1: return "CL_DEVICE_NOT_FOUND";
+	case -2: return "CL_DEVICE_NOT_AVAILABLE";
+	case -3: return "CL_COMPILER_NOT_AVAILABLE";
+	case -4: return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+	case -5: return "CL_OUT_OF_RESOURCES";
+	case -6: return "CL_OUT_OF_HOST_MEMORY";
+	case -7: return "CL_PROFILING_INFO_NOT_AVAILABLE";
+	case -8: return "CL_MEM_COPY_OVERLAP";
+	case -9: return "CL_IMAGE_FORMAT_MISMATCH";
+	case -10: return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
+	case -11: return "CL_BUILD_PROGRAM_FAILURE";
+	case -12: return "CL_MAP_FAILURE";
+	case -13: return "CL_MISALIGNED_SUB_BUFFER_OFFSET";
+	case -14: return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
+	case -15: return "CL_COMPILE_PROGRAM_FAILURE";
+	case -16: return "CL_LINKER_NOT_AVAILABLE";
+	case -17: return "CL_LINK_PROGRAM_FAILURE";
+	case -18: return "CL_DEVICE_PARTITION_FAILED";
+	case -19: return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
+
+	// compile-time errors
+	case -30: return "CL_INVALID_VALUE";
+	case -31: return "CL_INVALID_DEVICE_TYPE";
+	case -32: return "CL_INVALID_PLATFORM";
+	case -33: return "CL_INVALID_DEVICE";
+	case -34: return "CL_INVALID_CONTEXT";
+	case -35: return "CL_INVALID_QUEUE_PROPERTIES";
+	case -36: return "CL_INVALID_COMMAND_QUEUE";
+	case -37: return "CL_INVALID_HOST_PTR";
+	case -38: return "CL_INVALID_MEM_OBJECT";
+	case -39: return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
+	case -40: return "CL_INVALID_IMAGE_SIZE";
+	case -41: return "CL_INVALID_SAMPLER";
+	case -42: return "CL_INVALID_BINARY";
+	case -43: return "CL_INVALID_BUILD_OPTIONS";
+	case -44: return "CL_INVALID_PROGRAM";
+	case -45: return "CL_INVALID_PROGRAM_EXECUTABLE";
+	case -46: return "CL_INVALID_KERNEL_NAME";
+	case -47: return "CL_INVALID_KERNEL_DEFINITION";
+	case -48: return "CL_INVALID_KERNEL";
+	case -49: return "CL_INVALID_ARG_INDEX";
+	case -50: return "CL_INVALID_ARG_VALUE";
+	case -51: return "CL_INVALID_ARG_SIZE";
+	case -52: return "CL_INVALID_KERNEL_ARGS";
+	case -53: return "CL_INVALID_WORK_DIMENSION";
+	case -54: return "CL_INVALID_WORK_GROUP_SIZE";
+	case -55: return "CL_INVALID_WORK_ITEM_SIZE";
+	case -56: return "CL_INVALID_GLOBAL_OFFSET";
+	case -57: return "CL_INVALID_EVENT_WAIT_LIST";
+	case -58: return "CL_INVALID_EVENT";
+	case -59: return "CL_INVALID_OPERATION";
+	case -60: return "CL_INVALID_GL_OBJECT";
+	case -61: return "CL_INVALID_BUFFER_SIZE";
+	case -62: return "CL_INVALID_MIP_LEVEL";
+	case -63: return "CL_INVALID_GLOBAL_WORK_SIZE";
+	case -64: return "CL_INVALID_PROPERTY";
+	case -65: return "CL_INVALID_IMAGE_DESCRIPTOR";
+	case -66: return "CL_INVALID_COMPILER_OPTIONS";
+	case -67: return "CL_INVALID_LINKER_OPTIONS";
+	case -68: return "CL_INVALID_DEVICE_PARTITION_COUNT";
+
+	// extension errors
+	case -1000: return "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
+	case -1001: return "CL_PLATFORM_NOT_FOUND_KHR";
+	case -1002: return "CL_INVALID_D3D10_DEVICE_KHR";
+	case -1003: return "CL_INVALID_D3D10_RESOURCE_KHR";
+	case -1004: return "CL_D3D10_RESOURCE_ALREADY_ACQUIRED_KHR";
+	case -1005: return "CL_D3D10_RESOURCE_NOT_ACQUIRED_KHR";
+	default: return "Unknown OpenCL error";
+	}
+}
+
+#define CHECK_EC if (ec != CL_SUCCESS) printf("Line %i: %s\n", __LINE__, clGetErrorString(ec))
+
+int main(void)
+{
+	const char *src =
+		R"(
+__kernel void hello(__global char* string)
+{
+	string[0]  = 'H';
+	string[1]  = 'e';
+	string[2]  = 'l';
+	string[3]  = 'l';
+	string[4]  = 'o';
+	string[5]  = ',';
+	string[6]  = ' ';
+	string[7]  = 'W';
+	string[8]  = 'o';
+	string[9]  = 'r';
+	string[10] = 'l';
+	string[11] = 'd';
+	string[12] = '!';
+	string[13] = '\0';
+}
+)";
+	size_t srcSz = strlen(src);
+
+	cl_uint nPlatforms;
+	cl_platform_id platform;
+	cl_device_id device;
+	cl_uint nDevices;
+	cl_int ec;
+
+	clGetPlatformIDs(1, &platform, &nPlatforms);
+	clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, &nDevices);
+
+	cl_context context = clCreateContext(NULL, 1, &device, NULL, NULL, &ec); CHECK_EC;
+	cl_command_queue q = clCreateCommandQueue(context, device, 0, &ec); CHECK_EC;
+
+	cl_mem buff = clCreateBuffer(context, CL_MEM_READ_WRITE, 16, NULL, &ec); CHECK_EC;
+
+	cl_program prog = clCreateProgramWithSource(context, 1, &src, &srcSz, &ec); CHECK_EC;
+	ec = clBuildProgram(prog, 1, &device, NULL, NULL, NULL); CHECK_EC;
+	if (ec == CL_BUILD_PROGRAM_FAILURE)
+	{
+		size_t sz;
+		clGetProgramBuildInfo(prog, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &sz);
+
+		char *log = (char *)malloc(sz);
+		clGetProgramBuildInfo(prog, device, CL_PROGRAM_BUILD_LOG, sz, log, NULL);
+		puts(log);
+	}
+
+	cl_kernel kernel = clCreateKernel(prog, "hello", &ec); CHECK_EC;
+	ec = clSetKernelArg(kernel, 0, sizeof(buff), &buff); CHECK_EC;
+	ec = clEnqueueTask(q, kernel, 0, NULL, NULL); CHECK_EC;
+	
+	char str[16] = {};
+	ec = clEnqueueReadBuffer(q, buff, CL_TRUE, 0, 16, str, 0, NULL, NULL); CHECK_EC;
+	puts(str);
+
+	ec = clFlush(q); CHECK_EC;
+	ec = clFinish(q); CHECK_EC;
+	ec = clReleaseKernel(kernel); CHECK_EC;
+	ec = clReleaseProgram(prog); CHECK_EC;
+	ec = clReleaseMemObject(buff); CHECK_EC;
+	ec = clReleaseCommandQueue(q); CHECK_EC;
+	ec = clReleaseContext(context); CHECK_EC;
+
+	(void)getchar();
+}
